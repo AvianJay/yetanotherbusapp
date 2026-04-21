3810cb236a557413bdfefcc2c6d648d2d655fd3c
AvianJay <jay20090728@gmail.com>
2026-04-21 10:02:24 +0800

    fix

--- lib/screens/main_transit_shell.dart
+++ lib/screens/main_transit_shell.dart
@@ -17,36 +17,69 @@ class MainTransitShell extends StatefulWidget {
   State<MainTransitShell> createState() => _MainTransitShellState();
 }
 
-class _MainTransitShellState extends State<MainTransitShell> {
+class _MainTransitShellState extends State<MainTransitShell>
+    with SingleTickerProviderStateMixin {
   TransitMode _currentMode = TransitMode.bus;
 
+  late final AnimationController _fadeController;
+  late final Animation<double> _fadeAnimation;
+
+  static const _visibleModes = [
+    TransitMode.bus,
+    TransitMode.thsr,
+    TransitMode.tra,
+    TransitMode.youbike,
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _fadeController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 150),
+      value: 1.0,
+    );
+    _fadeAnimation = CurvedAnimation(
+      parent: _fadeController,
+      curve: Curves.easeInOut,
+    );
+  }
+
+  @override
+  void dispose() {
+    _fadeController.dispose();
+    super.dispose();
+  }
+
   void _setMode(TransitMode mode) {
-    if (mode == TransitMode.metro) {
-      mode = TransitMode.bus;
-    }
-    if (mode != _currentMode) {
+    if (mode == TransitMode.metro) mode = TransitMode.bus;
+    if (mode == _currentMode) return;
+
+    _fadeController.reverse().then((_) {
+      if (!mounted) return;
       setState(() => _currentMode = mode);
-    }
+      _fadeController.forward();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    final visibleModes = [
-      TransitMode.bus,
-      TransitMode.thsr,
-      TransitMode.tra,
-      TransitMode.youbike,
-    ];
-    final currentIndex = visibleModes.indexOf(_currentMode);
+    final currentIndex = _visibleModes.indexOf(_currentMode);
 
-    return IndexedStack(
-      index: currentIndex >= 0 ? currentIndex : 0,
-      children: [
-        HomeScreen(onModeChanged: _setMode),
-        ThsrScreen(onModeChanged: _setMode),
-        TraScreen(onModeChanged: _setMode),
-        YouBikeScreen(onModeChanged: _setMode),
-      ],
+    return ColoredBox(
+      color: Theme.of(context).scaffoldBackgroundColor,
+      child: FadeTransition(
+        opacity: _fadeAnimation,
+        child: IndexedStack(
+          index: currentIndex >= 0 ? currentIndex : 0,
+          children: [
+            HomeScreen(onModeChanged: _setMode),
+            ThsrScreen(onModeChanged: _setMode),
+            TraScreen(onModeChanged: _setMode),
+            YouBikeScreen(onModeChanged: _setMode),
+          ],
+        ),
+      ),
     );
   }
 }

f9a7eee2ddb4dfb0053c47825e2d7eaa7a3832b2
AvianJay <jay20090728@gmail.com>
2026-05-29 13:17:51 +0800

    fix

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -162,7 +162,6 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
 
     return Scaffold(
       body: SafeArea(
-        bottom: false,
         child: Padding(
           padding: const EdgeInsets.fromLTRB(20, 16, 20, 0),
           child: Column(
